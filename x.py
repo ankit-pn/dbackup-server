7f8037e0c6a5b9c80b7257d2929c4fbb25186f9d
ankit-pn <miankitsingh@gmail.com>
2023-12-22 09:30:12 +0530

    fix: sort hcdata list

--- x.py
+++ x.py
@@ -15,7 +15,7 @@ import os
 import csv
 import pytz
 from convert_zip import convert_to_zip
-
+from operator import itemgetter
 
 def get_scope():
     with open('config.yaml','r') as file:
@@ -436,11 +436,9 @@ def generate_csv():
 
 def generate_hcsv():
     filename = "history.csv"
-    # Connect to credentials.db
     conn_credentials = sqlite3.connect("credentials.db")
     cursor_credentials = conn_credentials.cursor()
 
-    # SQL JOIN Query to get unique folders with the latest backup date
     join_query = """
     SELECT hc.folder, hc.youtube_watched, hc.browser_history, MAX(f.last_backup) as last_backup
     FROM history_counts hc
@@ -450,39 +448,18 @@ def generate_hcsv():
     cursor_credentials.execute(join_query)
     records = cursor_credentials.fetchall()
 
-    # Get field names for the CSV file
     field_names = [desc[0] for desc in cursor_credentials.description]
     cursor_credentials.close()
     conn_credentials.close()
 
-    # Write data to CSV file
+    # Sort the records based on the last_backup column
+    sorted_records = sorted(records, key=itemgetter(3), reverse=True)  # Change to reverse=False for ascending order
+
+    # Write sorted data to CSV file
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(field_names)  # Write the header
-        writer.writerows(records)  # Write the records
-
-    return filename
-
-
-
-def generate_requests_csv():
-    filename = "requests_data.csv"
-    conn = sqlite3.connect("credentials.db")
-    cursor = conn.cursor()
-
-    # Execute SQL command to get all records from requests
-    cursor.execute("SELECT * FROM requests")
-    records = cursor.fetchall()
-    field_names = [desc[0] for desc in cursor.description]
-
-    # Close the connection
-    cursor.close()
-    conn.close()
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(field_names)  # Write the header
-        writer.writerows(records)  # Write the records
+        writer.writerow(field_names)
+        writer.writerows(sorted_records)
 
     return filename
 
