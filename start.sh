8606808add7266703c113eb68b455e4ec750fbce
AYUSHMAN-PANDA <panda.ayushman@gmail.com>
2025-04-23 04:54:18 -0400

    fix: added mapping to chatgpt folder to fastapi server

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 docker-compose down &&
 docker rmi -f dbackup-server_fastapi-server dbackup-server_scheduler &&
 git pull &&
-docker-compose up &&
+docker-compose up -d &&
 echo "All commands executed successfully!"
